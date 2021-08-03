f92f5858d325a685d5af6286b2d70f6819648b70
41212a <a41212a@naver.com>
2021-08-03 23:27:43 +0900

    1152 김윤하

---
+++ Bronze/1152/김윤하.java
@@ -0,0 +1,19 @@
+package b2_1152;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int answer = 0;
+		
+		while(st.hasMoreTokens()) {
+			st.nextToken();
+			answer++;
+		}
+		System.out.println(answer);
+	}
+}
