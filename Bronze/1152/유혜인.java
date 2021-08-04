a72a63e9e05913451617ea16e003afcb218ad02a
hain9563 <32196586+hain9563@users.noreply.github.com>
2021-08-04 23:09:59 +0900

    08.04 단어의 개수

---
+++ Bronze/1152/유혜인.java
@@ -0,0 +1,15 @@
+// 20012KB 296ms
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		System.out.print(st.countTokens());
+	}
+}
