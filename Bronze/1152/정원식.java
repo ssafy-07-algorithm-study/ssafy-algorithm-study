6b441d0aaa5c897d9d73f38030d71548bbc1b43e
wonsik3686 <70436955+wonsik3686@users.noreply.github.com>
2021-08-06 01:21:52 +0900

    08.05 add file

---
+++ Bronze/1152/정원식.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 정원식 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+
+		System.out.println(st.countTokens());
+	}
+}
