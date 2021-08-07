aae5b0fe83d8b19d33019a3a91daa87a90fa963c
LEE MINJUNG <3sally@naver.com>
2021-08-08 01:02:03 +0900

    //메모리 14472kb // 시간 144ms

---
+++ 21_08_13/2750/이민정.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class BOJ_2750_수정렬하기 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine()); // 몇개의 수를 읽을지
+		int[] arr = new int[N]; // N개 크기의 배열 생성
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(arr);
+		; // 배열 오름차순 정렬
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
