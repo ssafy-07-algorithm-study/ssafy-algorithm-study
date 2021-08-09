a61f4ff7622426b9459041c4778574468069ed42
hain9563 <32196586+hain9563@users.noreply.github.com>
2021-08-09 20:50:41 +0900

    Create 유혜인.java
    19984 / 292

---
+++ 21_08_13/20154/유혜인.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이구역의승자는누구야 {
+
+	public static void main(String[] args) throws IOException {
+		int[] alpha = { 3, 2, 1, 2, 3, 3, 3, 3, 1, 1, 3, 1, 3, 3, 1, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2, 1 };
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String a = br.readLine();
+		int len = a.length();
+		int sum = 0;
+		for(int i=0;i<len;i++) {
+			sum += alpha[a.charAt(i)- 65];
+		}
+		if(sum %2 == 0) {
+			System.out.println("You're the winner?");
+		}else {
+			System.out.println("I'm a winner!");
+		}
+	}
+
+}
