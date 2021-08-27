585e24bb68c7d39b8c2d3a5ebbd8d6705c3a216b
41212a <72211590+41212a@users.noreply.github.com>
2021-08-27 20:53:21 +0900

    Update 김윤하 (23552KB 324ms).java

--- 21_08_27/4779/김윤하 (23552KB 324ms).java
+++ 21_08_27/4779/김윤하 (23552KB 324ms).java
@@ -1,5 +1,34 @@
-package s3_18429_근손실;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String number;
+		while((number = br.readLine()) != null) {
+			int N = Integer.parseInt(number);
+			StringBuilder sb = new StringBuilder();
+			divide(sb, N);
+			System.out.println(sb);
+		}
+		
+		
+	}
+	
+	static void divide(StringBuilder sb, Integer N) {
+		if (N == 0) {
+			sb.append("-");
+			return;
+		}
+		
+		divide(sb, N-1);
+		int pow = 1;
+		for (int j = 0; j < N-1; j++)
+			pow *= 3;
+		for(int i = 0 ; i < pow; i++)
+			sb.append(" ");
+		divide(sb, N-1);
+	}
 }
