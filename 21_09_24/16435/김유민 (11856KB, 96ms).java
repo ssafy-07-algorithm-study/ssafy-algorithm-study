39bf1ffc8343052465d9c42529ce55e9c9272da8
yumin <49578987+yumin25@users.noreply.github.com>
2021-09-23 21:28:45 +0900

    Create 김유민 (11856KB, 96ms).java

---
+++ 21_09_24/16435/김유민 (11856KB, 96ms).java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int []arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) arr[i]=Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+		for(int i=0;i<N;i++) {
+			if(arr[i]>L) break;
+			L++;
+		}
+		System.out.println(L);
+	}
+}
